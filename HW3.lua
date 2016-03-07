fe028038fd8f39e19a9ab403eaf33a473a490ed2
Rohil Prasad <r0hil.pras4d@gmail.com>
2016-03-06 22:01:37 -0500

    Fix preprocessing, add MLE, laplace, and Witten-Bell (may be bugged still)

--- HW3.lua
+++ HW3.lua
@@ -12,6 +12,9 @@ cmd:option('-datafile', '', 'data file')
 cmd:option('-action', 'train', 'train or test')
 cmd:option('-smoothing', '', 'smoothing method')
 cmd:option('-lm', 'mle', 'classifier to use: mle, laplace, NNLM')
+cmd:option('-cm_out_name', '0', 'output file name of count matrix [set to 0 to not save]')
+cmd:option('-bigram_cm', '', 'path to precomputed bigram count matrix')
+cmd:option('-trigram_cm', '', 'path to precomputed trigram count matrix')
 
 cmd:option('-window_size', 5, 'window size')
 cmd:option('-warm_start', '', 'torch file with previous model')
@@ -30,53 +33,188 @@ cmd:option('-embed', 50, 'size of word embeddings')
 cmd:option('-hidden', 100, 'size of hidden layer for neural network')
 cmd:option('-skip_connect', 1, 'use skip connections in NNLM')
 
+function tbllength(T)
+  -- Get length of table
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
+
+function tblsum(T)
+  -- Get sum of table values
+  local total = 0
+  for key, value in pairs(T) do total = total + value end
+  return value
+end
+
+function hash(context, nclasses)
+  -- Hashes ngram context for 
+  local total = 0
+  for i = 1, context:size(1) do
+    total = total + (context[i] - 1) * (nclasses ^ (i-1))
+  end
+  return total
+end
+
+function unhash(X_idx, nclasses, ngram_size)
+  -- Converts index to ngram context
+  local X = torch.zeros(X_idx:size(1), ngram_size - 1)
+  for i = 1, X:size(1) do
+      idx = X_idx[i][1]
+    for j = 1, ngram_size - 1 do
+      X[i][j] = math.mod(idx, nclasses) + 1
+      idx = (idx - context[i]) / nclasses
+    end
+  end
+  return X
+end
+
 function make_count_matrix(X, Y, nclasses)
   -- Construct count matrix
   local CM = {}
   for i = 1, X:size(1) do
-    prefix = X[i]
+    prefix = hash(X[i], nclasses)
     if CM[prefix] == nil then
-      CM[prefix] = torch.zeros(1, nclasses)
+      CM[prefix] = {}
       word = Y[i]
-      CM[prefix][word - 1] = CM[prefix][word - 1] + 1
+      CM[prefix][word] = 1
     else
       word = Y[i]
-      CM[prefix][word - 1] = CM[prefix][word - 1] + 1
+      if CM[prefix][word] == nil then
+        CM[prefix][word] = 1
+      else
+        CM[prefix][word] = CM[prefix][word] + 1
+      end
     end
+  end
+  if opt.cm_out_name ~= '0' then
+    local output = opt.cm_out_name .. '.t7'
+    torch.save(output, CM)
   end
 
   return CM
 end
 
-function mle_preds(X, CM, nclasses)
-  preds = torch.zeroes(X:size(1), nclasses)
+function predict_laplace(X, CM, queries, alpha)
+  -- Predict distribution of the word following X[i] over queries[i]
+  local preds = torch.zeros(X:size(1), queries:size(2)) 
   for i = 1, X:size(1) do
-    prefix = X[i]
+    prefix = hash(X[i], nclasses)
     -- Return uniform distribution if nil
     if CM[prefix] == nil then
-      preds[i]:fill(1/nclasses)
+      preds[i]:fill(1/queries:size(2))
     else
-      preds[i] = CM[prefix]:div(CM[prefix]:sum())
+      for j = 1, queries[i]:size(1) do
+        preds[i][j] = alpha
+        if CM[prefix][queries[i][j]] ~= nil then
+          preds[i][j] = preds[i][j] + CM[prefix][queries[i][j]]
+        end
+      end
+      sum = preds[i]:sum()
+      if sum == 0 then
+        preds[i]:fill(1/queries:size(2))
+      else
+        preds[i]:div(sum)
+      end
     end
   end
 
   return preds
 end
 
-function laplace_preds(X, CM, alpha, nclasses)
-  preds = torch.zeroes(X:size(1), nclasses)
-  for i = 1, X:size(1) do
-    prefix = X[i]
-    -- Return uniform distribution if nil
-    if CM[prefix] == nil then
-      preds[i]:fill(1/nclasses)
-    else
-      CM[prefix]:add(alpha)
-      preds[i] = CM[prefix]:div(CM[prefix]:sum())
+function predict_witten_bell(X, bigram_CM, trigram_CM, nclasses, queries)
+  -- Predict distribution of the word following X[i] over queries[i]
+  local preds = torch.zeroes(X:size(1), queries:size(2))
+  local ngram_size = X:size(2) + 1 
+  if ngram_size == 2 then 
+    -- Calculate unigram counts
+    unigram_CM = {}
+    for bigram, suffixes in pairs(bigram_CM) do 
+      for unigram, count in pairs(suffixes) do
+        if unigram_CM[unigram] == nil then
+          unigram_CM[unigram] = count
+        else
+          unigram_CM[unigram] = unigram_CM[unigram] + count
+        end
+      end
+    end
+    total_unigram_count = tblsum(unigram_CM)
+    for i = 1, X:size(1) do
+      prefix = hash(X[i], nclasses)
+      if bigram_CM[prefix] == nil then
+        preds[i]:fill(1/queries:size(2))
+      else
+        unique_types = tbllength(bigram_CM[prefix])
+        total_bigram_count = tblsum(bigram_CM[prefix])
+        lambda = 1 - unique_types/(unique_types + total_count)
+        for j = 1, queries:size(2) do
+          if bigram_CM[prefix][queries[i][j]] ~= nil then
+            bigram_count = bigram_CM[prefix][queries[i][j]]
+          else
+            bigram_count = 0
+          end
+          if unigram_CM[queries[i][j]] ~= nil then
+            unigram_count = unigram_CM[queries[i][j]] or 0
+          else
+            unigram_count = 0
+          end
+          preds[i][j] = (bigram_count/total_bigram_count) * lambda + (unigram_count/total_unigram_count) * (1 - lambda)
+        end
+        sum = preds[i]:sum()
+        if sum == 0 then
+          preds[i]:fill(1/queries:size(2))
+        else
+          preds[i]:div(sum)
+        end
+      end
+    end
+  elseif ngram_size == 3 then
+    for i = 1, X:size(1) do
+      trigram_prefix = hash(X[i], nclasses)
+      bigram_prefix = math.mod(trigram_prefix, nclasses^2)
+      if trigram_CM[trigram_prefix] == nil then
+        preds[i]:fill(1/queries:size(2))
+      else
+        unique_types = tbllength(trigram_CM[trigram_prefix])
+        total_trigram_count = tblsum(trigram_CM[trigram_prefix])
+        if bigram_CM[bigram_prefix] ~= nil then
+          total_bigram_count = tblsum(bigram_CM[bigram_prefix])
+        else
+          total_bigram_count = 1
+        end
+        lambda = 1 - unique_types/(unique_types + total_trigram_count)
+        for j = 1, queries:size(2) do
+          if trigram_CM[trigram_prefix][queries[i][j]] ~= nil then
+            trigram_count = trigram_CM[trigram_prefix][queries[i][j]]
+          else
+            trigram_count = 0
+          end
+          if bigram_CM[bigram_prefix][queries[i][j]] ~= nil then
+            bigram_count = bigram_CM[bigram_prefix][queries[i][j]]
+          else
+            bigram_count = 0
+          end
+          preds[i][j] = (trigram_count/total_trigram_count) * lambda + (bigram_count/total_bigram_count) * (1 - lambda)
+        end
+        sum = preds[i]:sum()
+        if sum == 0 then
+          preds[i]:fill(1/queries:size(2))
+        else
+          preds[i]:div(sum)
+        end
+      end
     end
   end
-  
+
   return preds
+end
+
+function perplexity(preds)
+  local perp = torch.zeros(preds:size(1), 1)
+  for i = 1, preds:size(1) do
+    perp[i] = math.exp(preds[i]:log():mul(-1):mean())
+  end
+  return perp
 end
 
 function NNLM()
@@ -263,11 +401,14 @@ function main()
    local X_context = f:read('train_context'):all():long()
    local Y = f:read('train_output'):all():long()
    local valid_X = f:read('valid_input'):all():long()
-   local valid_X_context = f:read('valid_context'):all():long()
    local valid_Y = f:read('valid_output'):all():long()
-   local test_X = f:read('test_input'):all():long()
+   local valid_X_blanks = f:read('valid_blanks_input'):all():long()
+   local valid_X_queries = f:read('valid_blanks_queries'):all():long()
+   local valid_X_context = f:read('valid_context'):all():long()
+   local test_X = f:read('test_blanks_input'):all():long()
+   local test_X_queries = f:read('test_blanks_queries'):all():long()
+   local test_X_context = f:read('test_blanks_context'):all():long()
    nclasses = f:read('nclasses'):all():long()[1]
-   nfeatures = f:read('nfeatures'):all():long()[1]
    window_size = f:read('context_size'):all():long()[1]
 
 
@@ -275,12 +416,14 @@ function main()
    if opt.action == 'train' then
      print('Training...')
      if opt.lm == 'mle' then
+       print(X:size(1), X:size(2))
        CM = make_count_matrix(X, Y, nclasses)
-       preds = mle_preds(X, CM, nclasses) 
+       preds = predict_laplace(valid_X_blanks, CM, valid_X_queries, 0) 
      elseif opt.lm == 'laplace' then
        alpha = opt.alpha
        CM = make_count_matrix(X, Y, nclasses)
-       preds = laplace_preds(X, CM, alpha, nclasses)
+       preds = predict_laplace(valid_X_blanks, CM, valid_X_queries, alpha) 
+       print(perplexity(preds):mean())
      elseif opt.lm == 'NNLM' then
        train_model(X_context, Y, valid_X_context, valid_Y)
      end
--- preprocess.py
+++ preprocess.py
@@ -18,27 +18,7 @@ def get_word_ids(word_dict):
             word_to_idx[word] = int(id_num)
     return word_to_idx
 
-def get_ngram_vocab(file_list, word_to_idx, ngram_size, dataset):
-    ngram_to_idx = {}
-    ngram_idx = 1
-
-    for filename in file_list:
-        with open(filename, "r") as f:
-            for line in f:
-                line = line.rstrip()
-                words = line.split()
-                # Add start and end of sentence characters
-                words = ['<s>' for _ in range(ngram_size - 1)] + words
-                words = words + ['</s>']
-                for i in range(ngram_size-1, len(words)):
-                    ngram_prefix = tuple([word_to_idx[word] for word in words[i-ngram_size+1: i]])
-                    if ngram_prefix not in ngram_to_idx:
-                        ngram_to_idx[ngram_prefix] = ngram_idx
-                        ngram_idx += 1
-
-    return ngram_to_idx
-
-def convert_text(data_name, word_to_idx, ngram_to_idx, ngram_size, dataset, context_size=5):
+def convert_text(data_name, word_to_idx, ngram_size, dataset, context_size=5):
     ngram_features = []
     ngram_context = []
     ngram_lbls = []
@@ -56,12 +36,12 @@ def convert_text(data_name, word_to_idx, ngram_to_idx, ngram_size, dataset, cont
                 ngram_prefix = [word_to_idx[word] for word in words[i-ngram_size+1: i]]
                 context = [word_to_idx[word] for word in words[i-context_size:i]]
                 ngram_lbls.append(word_to_idx[ngram_suffix])
-                ngram_features.append(ngram_to_idx[tuple(ngram_prefix)])
+                ngram_features.append(ngram_prefix)
                 ngram_context.append(context)
 
     return np.array(ngram_features, dtype=np.int32), np.array(ngram_context, dtype=np.int32), np.array(ngram_lbls, dtype=np.int32)
 
-def convert_blanks(data_name, word_to_idx, ngram_to_idx, ngram_size, dataset, context_size=5):
+def convert_blanks(data_name, word_to_idx, ngram_size, dataset, context_size=5):
     ngram_features = []
     ngram_lbls = []
     ngram_queries = []
@@ -81,7 +61,7 @@ def convert_blanks(data_name, word_to_idx, ngram_to_idx, ngram_size, dataset, co
                 ngram = words[-ngram_size:]
                 ngram_prefix = [word_to_idx[word] for word in ngram[:-1]]
                 context = [word_to_idx[word] for word in words[-(context_size+1):-1]]
-                ngram_features.append(ngram_to_idx[tuple(ngram_prefix)])
+                ngram_features.append(ngram_prefix)
                 ngram_context.append(context)
                 ngram_suffix = ngram[-1:][0]
                 if ngram_suffix in word_to_idx:
@@ -121,25 +101,18 @@ def main(arguments):
     print 'Get word ids...'
     word_to_idx = get_word_ids(word_dict)
 
-    # Get ngram to id mapping
-    print 'Get ngram ids...'
-    ngram_to_idx = get_ngram_vocab([train, valid], word_to_idx, ngram_size, dataset)
-
     # Convert data
     print 'Processing data...'
-    train_input, train_context, train_output = convert_text(train, word_to_idx, ngram_to_idx, ngram_size, dataset, context_size)
+    train_input, train_context, train_output = convert_text(train, word_to_idx, ngram_size, dataset, context_size)
 
     if valid:
-        valid_input, valid_context, valid_output = convert_text(valid, word_to_idx, ngram_to_idx, ngram_size, dataset, context_size)
+        valid_input, valid_context, valid_output = convert_text(valid, word_to_idx, ngram_size, dataset, context_size)
 
     if valid_blanks:
-        valid_blanks_input, valid_blanks_output, valid_blanks_queries, valid_blanks_context = convert_blanks(valid_blanks, word_to_idx, ngram_to_idx, ngram_size, dataset, context_size)
+        valid_blanks_input, valid_blanks_output, valid_blanks_queries, valid_blanks_context = convert_blanks(valid_blanks, word_to_idx, ngram_size, dataset, context_size)
 
     if test_blanks:
-        test_blanks_input, _, test_blanks_queries, test_blanks_context = convert_blanks(test_blanks, word_to_idx, ngram_to_idx, ngram_size, dataset, context_size)
-
-    V = len(ngram_to_idx)
-    print('# of ngrams:', V)
+        test_blanks_input, _, test_blanks_queries, test_blanks_context = convert_blanks(test_blanks, word_to_idx, ngram_size, dataset, context_size)
 
     C = len(word_to_idx)
     print('# of unigrams:', C)
@@ -163,7 +136,6 @@ def main(arguments):
             f['test_blanks_input'] = test_blanks_input
             f['test_blanks_queries'] = test_blanks_queries
             f['test_blanks_context'] = test_blanks_context
-        f['nfeatures'] = np.array([V], dtype=np.int32)
         f['nclasses'] = np.array([C], dtype=np.int32)
         f['context_size'] = np.array([context_size], dtype=np.int32)
 
